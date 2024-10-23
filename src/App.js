24ba30df78eef0173481e1b6fedd3917568591cf
misogyu <avlis828@gmail.com>
2024-10-23 14:03:17 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    // Fetch data from JSON file
-    fetch('/data/all_player_data.json')
+    // Fetch data from JSON file located in the public directory
+    fetch('/data/final_player_data.json')
       .then((response) => response.json())
       .then((data) => setPlayers(data))
       .catch((error) => console.error('Error fetching data:', error));
@@ -22,37 +22,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Player Rankings</h1>
+        <h1>Final Player Rankings</h1>
         <input
           type="text"
           placeholder="Search players..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <table>
-          <thead>
-            <tr>
-              <th>PlayerID</th>
-              <th>FirstName</th>
-              <th>LastName</th>
-              <th>Event Name</th>
-              <th>Total Points</th>
-              <th>Rank</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredPlayers.map((player) => (
-              <tr key={player.PlayerID}>
-                <td>{player.PlayerID}</td>
-                <td>{player.FirstName}</td>
-                <td>{player.LastName}</td>
-                <td>{player["Event Name"]}</td>
-                <td>{player["Total Points"] || 'N/A'}</td>
-                <td>{player.Rank || 'N/A'}</td>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>PlayerID</th>
+                <th>FirstName</th>
+                <th>LastName</th>
+                <th>Event Name</th>
+                <th>Total Points</th>
+                <th>Junior National Points</th>
+                <th>Rank</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filteredPlayers.map((player) => (
+                <tr key={player.PlayerID}>
+                  <td>{player.PlayerID}</td>
+                  <td>{player.FirstName}</td>
+                  <td>{player.LastName}</td>
+                  <td>{player["Event Name"]}</td>
+                  <td>{player["Total Points"] || 'N/A'}</td>
+                  <td>{player["Junior National Points"] || 'N/A'}</td>
+                  <td>{player.Rank || 'N/A'}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </header>
     </div>
   );
